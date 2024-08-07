edbe7d82ec384f36ecfa213b145bfed47dfb8abd
eeemi <eeemula@gmail.com>
2024-08-07 17:27:27 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -95,8 +95,8 @@ vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<C-d>', '<C-d>zz') -- center after moving half page
 vim.keymap.set('n', '<C-u>', '<C-u>zz') -- center after moving half page
 
-vim.keymap.set('v', '<leader>y', '"+y') -- yank to system clipboard in *VISUAL* mode
 vim.keymap.set('n', '<leader>y', '"+y') -- yank to system clipboard in *NORMAL* mode
+vim.keymap.set('v', '<leader>y', '"+y') -- yank to system clipboard in *VISUAL* mode
 vim.keymap.set('n', '<leader>Y', '"+Y') -- yank cursor=>EOL to system clipboard in *NORMAL* mode
 vim.keymap.set('n', '<leader>yy', '_"+y$') -- yank line to system clipboard in *NORMAL* mode
 -- vim.keymap.set('n', '<leader>yy', '"+yg_') -- yank line to system clipboard in *NORMAL* mode
@@ -107,6 +107,11 @@ vim.keymap.set('n', '<leader>p', '"_dP') -- paste without changing the last yank
 -- vim.keymap.set('v', '<leader>P', '"+P') -- paste from system clipboard in *VISUAL* mode
 -- vim.keymap.set('n', '<leader>p', '"+p') -- paste from system clipboard in *NORMAL* mode
 -- vim.keymap.set('n', '<leader>P', '"+P') -- paste from system clipboard in *NORMAL* mode
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- Move selection line by line in visual mode
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv") -- Move selection line by line in visual mode
+
+vim.keymap.set('n', '<leader>R', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace current word in the whole file' })
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
